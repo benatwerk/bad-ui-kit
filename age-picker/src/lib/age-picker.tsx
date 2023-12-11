00ce6c579c8358b0ea3889dc23d18b424e802fb5
Benjamin Holland <benisjust@gmail.com>
2023-12-11 01:45:48 -0700

    Age picker was too easy, so made it harder to use, changes screenshot to include new age picker

--- age-picker/src/lib/age-picker.tsx
+++ age-picker/src/lib/age-picker.tsx
@@ -1,17 +1,40 @@
 import React, { useState, useMemo } from 'react';
 import styles from './age-picker.module.scss';
 
-const MIN_AGE = 0;
-const MAX_AGE = 200;
+const N_PRIMES = 62;
+
+function findPrimes(N: number) {
+  const limit = estimateUpperBoundForNthPrime(N);
+  const sieve = new Array(limit).fill(true);
+  const primes = [];
+
+  for (let num = 2; num < limit; num++) {
+    if (sieve[num]) {
+      primes.push(num);
+      if (primes.length === N) {
+        break;
+      }
+      for (let multiple = num * num; multiple < limit; multiple += num) {
+        sieve[multiple] = false;
+      }
+    }
+  }
+  return primes;
+}
+
+function estimateUpperBoundForNthPrime(N: number) {
+  if (N < 6) {
+    return 15;
+  }
+  return Math.ceil(N * (Math.log(N) + Math.log(Math.log(N))));
+}
 
 export const AgePicker: React.FC = () => {
   const [selectedAges, setSelectedAges] = useState<{ [age: number]: boolean }>(
     {}
   );
 
-  const ageRange = useMemo(() => {
-    return Array.from({ length: MAX_AGE - MIN_AGE + 1 }, (_, i) => i + MIN_AGE);
-  }, []);
+  const ageRange = findPrimes(N_PRIMES);
 
   const toggleAge = (age: number) => {
     setSelectedAges((prevSelectedAges) => ({
